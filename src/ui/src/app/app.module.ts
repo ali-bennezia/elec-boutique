003c4cc73b89bd40002604c531bcdff1e53c123d
ali-bennezia <bennezia.ali@gmail.com>
2024-06-24 15:06:39 +0200

    responsive navbar

--- src/ui/src/app/app.module.ts
+++ src/ui/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { NgIconsModule } from '@ng-icons/core';
 import { ionPersonOutline } from '@ng-icons/ionicons';
 
@@ -21,6 +22,10 @@ import { RegisterPageComponent } from './page/user/register-page/register-page.c
 import { RouterModule } from '@angular/router';
 import APP_ROUTES from './routing/routes';
 import { HomePageComponent } from './page/home-page/home-page.component';
+import { AboutPageComponent } from './page/legal/about-page/about-page.component';
+import { PrivacyPolicyPageComponent } from './page/legal/privacy-policy-page/privacy-policy-page.component';
+import { NotFoundPageComponent } from './page/not-found-page/not-found-page.component';
+import { MobileMenuComponent } from './layout/navbar/mobile-menu/mobile-menu.component';
 
 @NgModule({
   declarations: [
@@ -31,10 +36,15 @@ import { HomePageComponent } from './page/home-page/home-page.component';
     SigninPageComponent,
     RegisterPageComponent,
     HomePageComponent,
+    AboutPageComponent,
+    PrivacyPolicyPageComponent,
+    NotFoundPageComponent,
+    MobileMenuComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
--- src/ui/src/app/routing/routes.ts
+++ src/ui/src/app/routing/routes.ts
@@ -2,6 +2,9 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from '../page/home-page/home-page.component';
 import { SigninPageComponent } from '../page/user/signin-page/signin-page.component';
 import { RegisterPageComponent } from '../page/user/register-page/register-page.component';
+import { AboutPageComponent } from '../page/legal/about-page/about-page.component';
+import { PrivacyPolicyPageComponent } from '../page/legal/privacy-policy-page/privacy-policy-page.component';
+import { NotFoundPageComponent } from '../page/not-found-page/not-found-page.component';
 
 const APP_ROUTES: Routes = [
   {
@@ -16,6 +19,18 @@ const APP_ROUTES: Routes = [
   {
     path: 'inscription',
     component: RegisterPageComponent,
+  },
+  {
+    path: 'apropos',
+    component: AboutPageComponent,
+  },
+  {
+    path: 'politique-confidentialite',
+    component: PrivacyPolicyPageComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundPageComponent,
   },
 ];
 export default APP_ROUTES;
