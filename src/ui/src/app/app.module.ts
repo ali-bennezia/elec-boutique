8bc9ab2f2b8b428f9c19d95eb59c912d76f54daa
ali-bennezia <bennezia.ali@gmail.com>
2024-06-24 10:56:44 +0200

    navbar, search bar, auth UI preliminaries

--- src/ui/src/app/app.module.ts
+++ src/ui/src/app/app.module.ts
@@ -2,15 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NgIconsModule } from '@ng-icons/core';
+import { ionPersonOutline } from '@ng-icons/ionicons';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    NgIconsModule.withIcons({ ionPersonOutline }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
